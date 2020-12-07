c8891741d0ddb04f4f69051f8b7d6e920dae8468
Teofil Kostadinov <Totkata@users.noreply.github.com>
2020-12-07 12:34:35 +0200

    Changes to comments view and view model

--- Web/TRKPortfolio.Web.ViewModels/Comments/ViewModel/CommentViewModel.cs
+++ Web/TRKPortfolio.Web.ViewModels/Comments/ViewModel/CommentViewModel.cs
@@ -10,6 +10,10 @@
 
     public class CommentViewModel : IMapFrom<Comment>
     {
+        public int Id { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
         public string Text { get; set; }
 
         public IEnumerable<ReplyViewModel> Replies { get; set; }
